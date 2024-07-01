14012c2b5cd94d654d7fc237501e736cb52d77ed
infinity-boi <gouravsinghc2003@gmail.com>
2024-07-01 06:34:02 +0530

    Modified Inheritance

--- src/GS02_Inheritance/Test.java
+++ src/GS02_Inheritance/Test.java
@@ -5,7 +5,7 @@ package GS02_Inheritance;
 
 // driver class
 public class Test {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         MountainBike mb = new MountainBike(3, 100, 25);
         System.out.println("\nBefore Setting Height");
